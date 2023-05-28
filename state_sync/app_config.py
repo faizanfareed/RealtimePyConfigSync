ac40c4ef7d4c441baf5be171715b1cdab960b7b8
Faizan Fareed <faizan.fareed.2276@slashnext.net>
2023-05-28 12:26:30 +0500

    feat: Add app config module.

---
+++ state_sync/app_config.py
@@ -0,0 +1,9 @@
+import logging
+
+ADD_NEW_WATCH_CHANGES_ON_PREFIX = False
+SERIALIZATION_SUPPORT = ['JSON']
+
+# Logging details
+LOGGING_LEVEL = logging.DEBUG
+LOGGER_NAME = 'RealtimePyConfigSync'
+
